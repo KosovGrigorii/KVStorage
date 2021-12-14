58831bdc762b6020d1f7014083facd9e99f0584d
KosovGrigorii <72564996+KosovGrigorii@users.noreply.github.com>
2021-12-14 13:20:10 +0500

    Update main.py

--- KVStorage/main.py
+++ KVStorage/main.py
@@ -20,7 +20,6 @@ def get(table, key):
     filename = get_db_filename(key)
     with closing(SqliteDict(filename, autocommit=True)) as db:
         _check_valid_key(key, db)
-        print(db[key])
 
 
 def _check_valid_key(key, db):
@@ -33,21 +32,3 @@ def _check_valid_key(key, db):
 
 def get_db_filename(key):
     return f'./my_db{hash(key) % 33}.sqlite'
-
-
-# put('try13', 'omg let it be')
-# put('try14', 'omg let it be')
-# put('try17', ['omg let it be', 'omg let it be one more time'])
-# filename = get_db_filename('try17')
-# print(filename)
-keys = []
-# for i in range(100000):
-#     if hash(f'try{i}') % 33 == 8:
-#         #print(f'try{i}')
-#         keys.append(f'try{i}')
-#         put(f'try{i}', 'one more cell')
-#
-#
-# with closing(SqliteDict('./my_db8.sqlite', autocommit=True)) as db:
-#     for key in keys:
-#         get(db, key)
