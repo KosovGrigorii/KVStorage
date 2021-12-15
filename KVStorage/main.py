7d792b5f09b5045449106a4caf2e31751b6160d4
Grigorij_Kosov <kosov.gr@mail.ru>
2021-12-15 15:37:47 +0500

    Add refactored KVStorage

--- KVStorage/main.py
+++ KVStorage/main.py
@@ -1,34 +1,129 @@
-import pickle
 
+#import shelve
 from contextlib import closing
-
 from sqlitedict import SqliteDict
 
+HELP_TEXT = {"help": "show all possible commands and what each one suppose to do",
+             "h": "does the same thing as help",
+             "get": "returns a value using this key\nFormat: get <key>",
+             "put": "adding key value pair to a storage\nFormat: put <key>:<value>",
+             "help <command>": "shows command usage",
+             "exit": "exits the program"}
 
-def put(key, value):
 
-    filename = get_db_filename(key)
-    with closing(SqliteDict(filename, autocommit=True)) as db:
-        if type(value) is not list:
-            db[key] = [value]
+# try:
+#     key = command.split(' ')[1]
+#     value = command_components[1]
+#     put(key, value)
+#     print(f"Pair {key}: {value} is added to the storage")
+# except Exception:
+#
+#     print("Incorrect format. Check format by typing help or h")
+
+
+# def put(key, file_name):
+#     with shelve.open('storage') as storage:
+#         storage[key] = []
+#         with open(file_name) as f:
+#             storage[key] = f.readlines()
+#
+#
+# def get(key, file_name):
+#     with shelve.open('storage') as storage:
+#         with open(file_name, 'w') as f:
+#             f.write(*storage[key])
+
+
+def put(key, file_name):
+
+    with closing(SqliteDict('./my_db1.sqlite', tablename='filenames', autocommit=True)) as db:
+        db[key] = file_name
+
+    with closing(SqliteDict('./my_db1.sqlite', tablename='values', autocommit=True)) as db:
+        db[key] = []
+        with open(file_name) as f:
+            db[key] = f.readlines()
+
+
+def get(key):
+
+    file_name = ""
+    with closing(SqliteDict('./my_db1.sqlite', tablename='filenames', autocommit=True)) as db:
+        if check_valid_key(key, db):
+            file_name = db[key]
         else:
-            db[key] = value
+            return
+
+    with closing(SqliteDict('./my_db1.sqlite', tablename='values', autocommit=True)) as db:
+        if check_valid_key(key, db):
+            with open(file_name, 'w') as f:
+                f.writelines(db[key])
+                print(f)
 
 
-def get(table, key):
-
-    filename = get_db_filename(key)
-    with closing(SqliteDict(filename, autocommit=True)) as db:
-        _check_valid_key(key, db)
-
-
-def _check_valid_key(key, db):
+def check_valid_key(key, db):
 
     if not isinstance(key, str):
-        raise ValueError('%r is not a valid key type' % key)
+        print('key %r is not a valid key type' % key)
+        return False
+
     if key not in db:
-        raise ValueError('%r could not be found' % key)
+        print('key %r could not be found' % key)
+        return False
+
+    return True
 
 
-def get_db_filename(key):
-    return f'./my_db{hash(key) % 33}.sqlite'
+# def get_db_filename(key):
+#     return f'./my_db{hash(key) % 33}.sqlite'
+
+
+def print_help(command=None):
+    if command is not None:
+        print(HELP_TEXT[command])
+    else:
+        print("DOCUMENTATION\n--------------------------")
+        for key, value in HELP_TEXT.items():
+            print(f"{key}: {value}")
+            print()
+
+
+def execute_command(command_components):
+    command = command_components[0]
+    if command.startswith("get"):
+        key = command.split(' ')[1]
+        get(key)
+
+    elif command.startswith("put"):
+        try:
+            key = command.split(' ')[1]
+            value = command_components[1]
+            put(key, value)
+            parsed_value = value.split('/')
+            print(f"Pair {key}: {parsed_value[len(parsed_value) - 1]} is added to the storage")
+
+        except Exception as exep:
+            print(exep)
+            print("Incorrect format. Check format by typing help or h")
+
+    elif command == "help":
+
+        command_to_describe = None
+        if len(command_components[0].split(' ')) == 2:
+            command_to_describe = command_components[1]
+        print_help(command_to_describe)
+
+    elif command == "h":
+        print_help()
+
+
+def main():
+    while True:
+        command = input()
+        if command is None or command == "exit":
+            break
+        execute_command(command.split(':'))
+
+
+if __name__ == '__main__':
+    main()
